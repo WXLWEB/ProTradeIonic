b427adeec098086afbd7c882324a857d0475e743
WXLWEB <894147172@qq.com>
2016-01-19 11:49:30 +0800

    init the ionic

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ProTradeIonic.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('ProTradeIonic')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ProTradeIonic.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('ProTradeIonic')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
