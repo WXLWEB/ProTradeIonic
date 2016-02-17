d4203498a8d653643f81b9df91b0aee66acfe854
WXLWEB <894147172@qq.com>
2016-02-17 22:01:25 +0800

    add chart library

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -10,18 +10,4 @@ angular.module('ProTradeIonic')
   .controller('MainController', function($scope) {
 
     // do something with $scope
-    //  var widget = new TradingView.widget({
-    //    fullscreen: true,
-    //    symbol: 'BTCCNY',
-    //    interval: '60',
-    //    container_id: "tv_chart_container",
-    //    //	BEWARE: no trailing slash is expected in feed URL
-    //    datafeed: new Datafeeds.UDFCompatibleDatafeed("https://pro-data.btcc.com/data/udf"),
-    //    library_path: "charting_library/",
-    //    locale: "en",
-    //    //	Regression Trend-related functionality is not implemented yet, so it's hidden for a while
-    //    drawings_access: { type: 'black', tools: [ { name: "Regression Trend" } ] },
-    //    disabled_features: ["use_localstorage_for_settings"],
-    //    preset: "mobile"
-    //  });
   });
