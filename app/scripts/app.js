3d30090a2d0b54f15e5c0f7fd5e1405542580fca
WXLWEB <894147172@qq.com>
2016-02-18 20:57:31 +0800

    merge origin

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -53,6 +53,50 @@ angular.module('ProTradeIonic', ['ionic', 'ngCordova', 'ngResource'])
             controller: 'SettingsController'
           }
         }
+      })
+      // Each tab has its own nav history stack:
+
+      //.state('app.dash', {
+      //  url: '/dash',
+      //  cache: true,
+      //  views: {
+      //    'tab-dash': {
+      //      templateUrl: 'templates/views/tab-dash.html',
+      //      controller: 'DashCtrl'
+      //    }
+      //  }
+      //})
+
+      .state('app.chats', {
+        url: '/chats',
+        cache: true,
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/views/tab-chats.html',
+            controller: 'ChatsCtrl'
+          }
+        }
+      })
+      .state('app.chat-detail', {
+        url: '/chats/:chatId',
+        cache: true,
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/views/chat-detail.html',
+            controller: 'ChatDetailCtrl'
+          }
+        }
+      })
+
+      .state('app.account', {
+        url: '/account',
+        cache: true,
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/views/tab-account.html',
+            controller: 'AccountCtrl'
+          }
+        }
       });
 
 
