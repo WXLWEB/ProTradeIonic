aeffd1243bc91492989246e9c2795986476922ca
WXLWEB <894147172@qq.com>
2016-04-25 15:40:01 +0800

    add intro page

---
+++ src/app/components/intro/introController.js
@@ -0,0 +1,19 @@
+'use strict'
+angular.module('ProTradeIonic')
+  .controller('introController',function($scope, $state, $ionicSlideBoxDelegate) {
+    // Called to navigate to the main app
+    $scope.startApp = function() {
+      $state.go('app.home');
+    };
+    $scope.next = function() {
+      $ionicSlideBoxDelegate.next();
+    };
+    $scope.previous = function() {
+      $ionicSlideBoxDelegate.previous();
+    };
+
+    // Called each time the slide changes
+    $scope.slideChanged = function(index) {
+      $scope.slideIndex = index;
+    };
+});
